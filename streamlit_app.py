133163452b6c8a0310e50f5ae851ea82de8b7291
LI <114208039+LIVCODER@users.noreply.github.com>
2022-09-25 22:22:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 streamlit.title("My parents New Healthy Diner")
 streamlit.text("Omega 3 and Blueberry Oatmeal")
+streamlit.text("Kale, Spinach, and Rocket Smoothie")
+streamlit.text("Hard-boiled free range egg")
